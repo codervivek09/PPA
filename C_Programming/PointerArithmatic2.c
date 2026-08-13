331adc9379b3d7ff100a688ec88f3dede070e5fa
Vivek Phad <vivekphad775@gmail.com>
2026-08-13 20:26:53 +0530

    Add Files via Upload

---
+++ C_Programming/PointerArithmatic2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int Arr [] = {11,21,51,101,111};
+
+    int*P = NULL;
+    int*Q = NULL;
+
+    P = &(Arr[1]);
+    Q = &(Arr[3]);
+
+    printf("%d\n", *P);
+    printf("%d\n", *Q);
+
+    P++;
+    Q--;
+
+    printf("%d\n", *P);
+    printf("%d\n", *Q);
+
+    return 0;
+}
