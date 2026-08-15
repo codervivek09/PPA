4bf11fa17e7e7488d3f8ec5e80a929a9ea0625c6
Vivek Phad <vivekphad775@gmail.com>
2026-08-15 09:56:12 +0530

    Add Files via Upload

---
+++ C_Programming/Struct_Demo5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#pragma pack (1)
+
+// Declaration of a Structure
+struct Demo 
+{
+    // Error - Because Memory is not allocated here.
+    int i = 11;
+    char ch = 'A';
+    float f = 90.99f;
+
+};
+
+int main ()
+{
+    struct Demo dobj1;
+    struct Demo dobj2;
+
+    struct Demo*DP = NULL;
+
+    DP= &dobj2;
+
+    // Direct Accessing Operator
+    dobj1.i = 11;
+    dobj1.ch ='A';
+    dobj1.f= 90.99f;
+
+    // Indirect Accessing Operator
+    DP ->i = 21;
+    DP ->ch = 'B';
+    DP ->f = 91.99f;
+
+    printf("%d\n", dobj1.i);
+    printf("%d\n", DP->i);
+
+    return 0;
+}
