2d23b99cdcd7af75ea87f5460d0be23a03a13f14
Vivek Phad <vivekphad775@gmail.com>
2026-08-19 01:13:24 +0530

    Add Files via Upload

---
+++ C_Programming/StructureUnion1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+#pragma pack(1)
+struct Demo
+{
+    int i;
+    float f;
+    double d;
+
+};
+
+
+int main ()
+{
+    printf("%d\n", sizeof(struct Demo)); 
+
+    return 0;
+}
