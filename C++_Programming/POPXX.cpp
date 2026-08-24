d263c181b7bf7bc14a46c85b6e98cf003f81bd3a
Vivek Phad <vivekphad775@gmail.com>
2026-08-24 23:43:19 +0530

    Add Files via Upload

---
+++ C++_Programming/POPXX.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+struct Arithmatic
+{
+    int No1;
+    int No2;
+};
+
+int main ()
+{
+    Arithmatic Aobj1;
+    Arithmatic Aobj2;
+
+    Aobj1.No1 = 0;
+    Aobj1.No2 = 0;
+
+    Aobj2.No1 = 10;
+    Aobj2.No2 = 11;
+
+    return 0;
+}
