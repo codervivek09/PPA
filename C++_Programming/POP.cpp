1a0385ae1d8ec44078667fdaa550e6b766989b82
Vivek Phad <vivekphad775@gmail.com>
2026-08-24 23:42:35 +0530

    Add Files via Upload

---
+++ C++_Programming/POP.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int Value1 = 0, Value2 = 0, Result = 0;
+    
+    cout<<"Enter First No. : ";
+    cin>>Value1;
+
+    cout<<"Enter Second No. : ";
+    cin>>Value2;
+
+    Result = Value1 + Value2;
+
+    cout<<"Addition is : "<<Result<<"\n";
+
+    return 0;
+}
