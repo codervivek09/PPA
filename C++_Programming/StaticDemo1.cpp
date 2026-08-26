11e7bab46595d1f8dd46cb3a211265d38d62e178
Vivek Phad <vivekphad775@gmail.com>
2026-08-26 12:36:30 +0530

    Add Files via Upload

---
+++ C++_Programming/StaticDemo1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Demo 
+{
+    public:
+        int No1;
+        int No2;
+        static int X;
+
+};
+
+int main ()
+{
+    Demo Obj1;
+    Demo Obj2;
+
+    cout<<sizeof(Obj1);
+
+    return 0;
+}
