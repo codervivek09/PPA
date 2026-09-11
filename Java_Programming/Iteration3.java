82fd6deb25f0bae4c158661b25d2685e368c3e8d
Vivek Phad <vivekphad775@gmail.com>
2026-09-11 23:47:30 +0530

    Add Files via Upload

---
+++ Java_Programming/Iteration3.java
@@ -0,0 +1,14 @@
+class Iteration3
+{
+    public static void main(String A[]) 
+    {
+        int i = 0;
+        i = 1;      // 1
+
+        //       2
+        while(i<=4){
+            System.out.println(i + "Jay Gnaesh...!");         // 4
+            i++;                                            // 3
+        }
+    }    
+}
