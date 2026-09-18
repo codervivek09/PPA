98d6b3bf02e67a6a0138adf7580f480a848c7a3b
Vivek Phad <vivekphad775@gmail.com>
2026-09-18 14:58:19 +0530

    Add Files via Upload

---
+++ C++_Programming/SingleLevel.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i, j;
+
+        Base()
+        {
+            cout<<"Inside Base Constructor\n";
+        }
+
+        ~Base()
+        {
+            cout<<"Inside Base Destructor\n";
+        }
+
+        void fun()
+        {
+            cout<<"Inside Base Fun\n";
+        }
+
+        void gun()
+        {
+            cout<<"Inside Base Gun\n";
+        }
+
+};
+
+class Derived : public Base
+{
+    public:
+        int x, y;
+
+        Derived()
+        {
+            cout<<"Inside Derived Constructor\n";
+        }
+
+        ~Derived()
+        {
+            cout<<"Inside Derived Destructor\n";
+        }
+
+        void sun ()
+        {
+            cout<<"Inside Derived Sun\n";
+        }
+
+};
+
+int main ()
+{
+    cout<<sizeof(Base)<<"\n";
+    cout<<sizeof(Derived)<<"\n";
+
+    return 0;
+}
